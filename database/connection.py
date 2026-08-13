efee7fc5fc06a09a094abd38b2dd1ca6dbadd3b1
anushkashingade31-tech <anushkashingade31@gmail.com>
2026-08-13 23:13:26 +0530

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -1,27 +1,12 @@
-import os
 import mysql.connector
 import streamlit as st
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 def get_connection():
-
-    if "DB_HOST" in st.secrets:
-
-        return mysql.connector.connect(
-            host=st.secrets["DB_HOST"],
-            user=st.secrets["DB_USER"],
-            password=st.secrets["DB_PASSWORD"],
-            database=st.secrets["DB_NAME"],
-            port=int(st.secrets.get("DB_PORT", 3306))
-        )
-
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306))
+        host=st.secrets["DB_HOST"],
+        port=int(st.secrets["DB_PORT"]),
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        database=st.secrets["DB_NAME"],
+        ssl_ca=st.secrets["DB_SSL_CA"]
     )
