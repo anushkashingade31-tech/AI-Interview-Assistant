0e5bf45aa9570a56539db9c74e6d927cf63368a2
anushkashingade31-tech <anushkashingade31@gmail.com>
2026-09-06 19:55:29 +0530

    Update groq_client.py

--- ai/groq_client.py
+++ ai/groq_client.py
@@ -14,18 +14,25 @@ MODEL_NAME = "openai/gpt-oss-120b"
 def ask_groq(prompt):
 
     response = client.chat.completions.create(
-
         model=MODEL_NAME,
-
         messages=[
             {
                 "role": "user",
                 "content": prompt
             }
         ],
-
         temperature=0.3
-
     )
 
     return response.choices[0].message.content
+
+
+def transcribe_audio(audio_file):
+
+    transcription = client.audio.transcriptions.create(
+        file=audio_file,
+        model="whisper-large-v3-turbo",
+        response_format="json"
+    )
+
+    return transcription.text
